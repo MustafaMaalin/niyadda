6b68e9a6a08ca2cd894e47bf0829166d206400b2
Mustafa <mustimustafa5@gmail.com>
2023-09-07 22:08:39 +0200

    expertise change

--- components/Expertise.js
+++ components/Expertise.js
@@ -16,7 +16,7 @@ const Expertise = () => {
           </div>
           <div className='hero-content grid-4'>
             {expertise.map((item) => (
-              <Card data={item} key={item.id} path='services' caption='learn more' />
+              <Card data={item} key={item.id} path='#' caption='' />
             ))}
           </div>
         </div>
