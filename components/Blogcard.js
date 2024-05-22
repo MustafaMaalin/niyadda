29a4d72191e433c9bee41c2bf3cc01370d2a1679
Mustafa M <mustimustafa5@gmail.com>
2024-05-22 16:52:51 +0200

    Blogcase fix
     Sort the blogdata by ID in descending order

--- components/Blogcard.js
+++ components/Blogcard.js
@@ -1,18 +1,40 @@
-import React from "react"
-import { Card } from "./common/Card"
-import { blogdata } from "@/assets/data/dummydata"
-import Link from "next/link"
+// import React from "react"
+// import { Card } from "./common/Card"
+// import { blogdata } from "@/assets/data/dummydata"
+// import Link from "next/link"
+
+// const BlogCard = () => {
+//   return (
+//     <>
+//       <div className='container blog-card grid-2 py'>
+//         {blogdata.map((item) => (
+//           <Card data={item} key={item.id} path='blogs' />
+//         ))}
+//       </div>
+//     </>
+//   )
+// }
+
+// export default BlogCard
+
+import React from "react";
+import { Card } from "./common/Card";
+import { blogdata } from "@/assets/data/dummydata";
+import Link from "next/link";
 
 const BlogCard = () => {
+  // Sort the blogdata by ID in descending order
+  const sortedBlogData = blogdata.sort((a, b) => b.id - a.id);
+
   return (
     <>
-      <div className='container blog-card grid-2 py'>
-        {blogdata.map((item) => (
-          <Card data={item} key={item.id} path='blogs' />
+      <div className="container blog-card grid-2 py">
+        {sortedBlogData.map((item) => (
+          <Card data={item} key={item.id} path="blogs" />
         ))}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default BlogCard
+export default BlogCard;
