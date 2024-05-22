94db4217de73a168511cc2a7d616821fd088a460
Mustafa M <mustimustafa5@gmail.com>
2024-05-22 16:36:18 +0200

    Showcase fix
    Sort the showcase data by ID in descending order

--- components/Showcard.js
+++ components/Showcard.js
@@ -1,19 +1,42 @@
-import { showcase } from "@/assets/data/dummydata"
-import { Card } from "@/components/common/Card"
-import { Title, TitleSm } from "@/components/common/Title"
-import React from "react"
-import Link from "next/link"
+// import { showcase } from "@/assets/data/dummydata"
+// import { Card } from "@/components/common/Card"
+// import { Title, TitleSm } from "@/components/common/Title"
+// import React from "react"
+// import Link from "next/link"
+
+// const Showcard = () => {
+//   return (
+//     <>
+//         <div className='container showcase grid-2 py'>
+//           {showcase.map((item) => (
+//             <Card data={item} key={item.id} path='cases' />
+//           ))}
+//         </div>
+//     </>
+//   )
+// }
+
+// export default Showcard
+
+import { showcase } from "@/assets/data/dummydata";
+import { Card } from "@/components/common/Card";
+import { Title, TitleSm } from "@/components/common/Title";
+import React from "react";
+import Link from "next/link";
 
 const Showcard = () => {
+  // Sort the showcase data by ID in descending order
+  const sortedShowcase = showcase.sort((a, b) => b.id - a.id);
+
   return (
     <>
-        <div className='container showcase grid-2 py'>
-          {showcase.map((item) => (
-            <Card data={item} key={item.id} path='cases' />
-          ))}
-        </div>
+      <div className="container showcase grid-2 py">
+        {sortedShowcase.map((item) => (
+          <Card data={item} key={item.id} path="cases" />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Showcard
+export default Showcard;
