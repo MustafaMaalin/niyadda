e7a1e161f79ab6119d715d047223118a010c2d02
Mustafa M <mustimustafa5@gmail.com>
2024-06-21 00:10:16 +0200

    Card url is clickable

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,58 +1,3 @@
-// import { TitleSm } from "./Title";
-// import { HiOutlineArrowRight } from "react-icons/hi";
-// import { useRouter } from "next/router";
-// import { slugify } from "assets/utils/slugify"; // Adjust the import path as needed
-
-// export const Card = ({ data, caption, show, path }) => {
-//   const router = useRouter();
-//   const slug = slugify(data.title);
-
-//   return (
-//     <div className='card'>
-//       <div className='card-img'>
-//         <img src={data.cover} alt={data.title} />
-//       </div>
-//       <div className='card-details'>
-//         <div
-//           className='title-link'
-//           onClick={() => {
-//             if (path && slug) {
-//               router.push(`${path}/${slug}`);
-//             }
-//           }}
-//           style={{ cursor: path ? 'pointer' : 'default' }}
-//         >
-//           <TitleSm title={data.title} />
-//         </div>
-//         {caption && (
-//           <div
-//             onClick={() => {
-//               if (path && slug) {
-//                 router.push(`${path}/${slug}`);
-//               }
-//             }}
-//             style={{ cursor: path ? 'pointer' : 'default' }}
-//           >
-//             <div>
-//               {caption} <HiOutlineArrowRight className='link-icon' />
-//             </div>
-//           </div>
-//         )}
-//         <div className='flex'>
-//           <span> {data.catgeory} </span> {data.date && <span> / {data.date}</span>}
-//         </div>
-//         {show && (
-//           <ul>
-//             {data.desc.map((text, i) => (
-//               <li key={i}> - {text.text}</li>
-//             ))}
-//           </ul>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
 import { TitleSm } from "./Title";
 import { HiOutlineArrowRight } from "react-icons/hi";
 import { useRouter } from "next/router";
@@ -60,30 +5,32 @@ import { useRouter } from "next/router";
 export const Card = ({ data, caption, show, path }) => {
   const router = useRouter();
 
+  const handleNavigation = () => {
+    if (path && data.id) {
+      router.push(`${path}/${data.id}`);
+    }
+  };
+
   return (
     <div className='card'>
-      <div className='card-img'>
+      <div 
+        className='card-img'
+        onClick={handleNavigation}
+        style={{ cursor: path ? 'pointer' : 'default' }}
+      >
         <img src={data.cover} alt={data.title} />
       </div>
       <div className='card-details'>
         <div
           className='title-link'
-          onClick={() => {
-            if (path && data.id) {
-              router.push(`${path}/${data.id}`);
-            }
-          }}
+          onClick={handleNavigation}
           style={{ cursor: path ? 'pointer' : 'default' }}
         >
           <TitleSm title={data.title} />
         </div>
         {caption && (
           <div
-            onClick={() => {
-              if (path && data.id) {
-                router.push(`${path}/${data.id}`);
-              }
-            }}
+            onClick={handleNavigation}
             style={{ cursor: path ? 'pointer' : 'default' }}
           >
             <div>
